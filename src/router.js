2e8a25a1980cb0efd8155f25d3450c8948412ded
yasin <yasinmrvvj@gmail.com>
2024-07-31 15:03:31 +0330

    create login page and add to routes file

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
+import LoginPage from "@/pages/LoginPage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {path: '/', component: LoginPage, name: "LoginPage"}
+    ]
 });
 
 export default router;
