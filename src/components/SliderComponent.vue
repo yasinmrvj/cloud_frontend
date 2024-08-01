5cae88c165305bdad01a2ee846053b020dbcd6f0
yasinmrvj <yasinmrvvj@gmail.com>
2024-08-01 16:28:39 +0330

    slider component edit

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -1,11 +1,109 @@
 <script setup>
+import {Swiper, SwiperSlide} from "swiper/vue";
+import {Pagination, Autoplay} from "swiper/modules";
+import {EffectFade} from "swiper/modules";
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 </script>
 
 <template>
-
+  <div class="w-full h-full">
+    <Swiper :pagination="true" :autoplay="{delay:10000,disableOnInteraction:true}" :loop="true"
+            :modules="[Pagination,Autoplay,EffectFade]" :effect="'fade'"
+            class="h-full mb-4">
+      <SwiperSlide class="flex flex-col items-center justify-center bg-sky-400 ">
+        <div class="w-[390px] h-[400px] flex justify-center items-center animate-[float_10s_ease-in-out_infinite] ">
+          <img src="/os.png" class="w-full "
+               alt="">
+        </div>
+        <img src="/emoji1.png"
+             class="absolute top-[440px] right-[51px] animate-[float1_8s_ease-in-out_infinite]" alt="">
+        <img src="/emoji2.png"
+             class="absolute top-[160px] left-[51px] rotate-[15deg] animate-[float2_8s_ease-in-out_infinite] opacity-90"
+             alt="">
+        <p class="font-[700] text-white/90">انتخاب سیستم عامل دلخواه</p>
+      </SwiperSlide>
+      <SwiperSlide class="flex flex-col items-center justify-center bg-red-500">
+        <div class="w-[390px] h-[400px] flex justify-center items-center  ">
+          <img src="/other.png" class="w-[180px] ml-4 animate-[float3_10s_ease-in-out_infinite]"
+               alt="">
+          <img src="/graphic.png" class="w-[180px] animate-[float4_10s_ease-in-out_infinite]"
+               alt="">
+        </div>
+        <p class="font-[700] text-white/90">اپلیکیشن های دلخواه</p>
+      </SwiperSlide>
+    </Swiper>
+  </div>
 </template>
 
-<style scoped>
+<style>
+.swiper-pagination-bullet {
+  @apply w-[10px] h-[10px] bg-white rounded-full
+}
 
+.swiper-pagination-bullet-active {
+  @apply w-[18px]
+}
+
+@keyframes float {
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(5px, 20px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+
+@keyframes float1 {
+  0% {
+    transform: translate(0, 0) rotate(15deg);
+  }
+  50% {
+    transform: translate(-15px, -10px) rotate(15deg);
+  }
+  100% {
+    transform: translate(0, 0) rotate(15deg);
+  }
+}
+
+@keyframes float2 {
+  0% {
+    transform: translate(0, 0) rotate(-15deg);
+  }
+  50% {
+    transform: translate(10px, 20px) rotate(-15deg);
+  }
+  100% {
+    transform: translate(0, 0) rotate(-15deg);
+  }
+}
+
+@keyframes float3 {
+  0% {
+    transform: translate(0, 0) rotate(10deg);
+  }
+  50% {
+    transform: translate(-5px, 20px) rotate(10deg);
+  }
+  100% {
+    transform: translate(0, 0) rotate(10deg);
+  }
+}
+
+@keyframes float4 {
+  0% {
+    transform: translate(0, 5px) rotate(10deg);
+  }
+  50% {
+    transform: translate(10px, -35px) rotate(10deg);
+  }
+  100% {
+    transform: translate(0, 5px) rotate(10deg);
+  }
+}
 </style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,47 @@
 <script setup>
+import {Field, Form} from "vee-validate";
+import SliderComponent from "@/components/SliderComponent.vue";
+
 
 </script>
 
 <template>
+  <div class="w-screen min-h-screen bg-[#F5F5F5] flex justify-center items-center">
+    <section class="w-[950px] h-[654px] bg-white rounded-xl flex overflow-hidden shadow-[0_0_30px_rgba(0,0,0,0.04)]">
+      <!--   form   -->
+      <div class="w-6/12 flex flex-col gap-6 items-center justify-center relative">
+        <h1 class="text-xl font-[500]">پنل کاربری سرویس های ابری</h1>
+        <!--  form  -->
+        <Form>
+          <!--   wrapper input   -->
+          <div class="flex flex-col gap-6 mt-6">
+            <Field name="username"
+                   class="bg-gray-50 py-2 rounded-md w-[300px] px-4 border-2 transition-all duration-300 border-gray-100 focus:border-blue-500 outline-none "
+                   autofocus
+                   placeholder="نام کاربری"/>
+            <Field name="password" type="password"
+                   class="bg-gray-50 py-2 rounded-md w-[300px] px-4 border-2 transition-all duration-300 border-gray-100 focus:border-blue-500 outline-none "
+                   placeholder="کلمه عبور"/>
+            <button type="submit"
+                    class="bg-[#333] hover:bg-[#444] transition-colors duration-300 py-3 text-white rounded-md">ورود به
+              سرویس ابری
+            </button>
+          </div>
+        </Form>
+        <!--  team  -->
+        <div class="absolute bottom-8 text-center flex flex-col gap-2 text-xs">
+          <p class="text-black/70">گروه برنامه نویسی فرا ارتباط</p>
+          <p class="text-black/70">درصورت بروز هرگونه مشکل با پشتیبانی تماس بگیرید.</p>
+          <p class="text-black/70">051-31668185</p>
 
+        </div>
+      </div>
+      <!--   slider   -->
+      <div class="w-6/12">
+        <SliderComponent/>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
